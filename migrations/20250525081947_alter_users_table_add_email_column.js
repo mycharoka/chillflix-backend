eca66c18fb3b6734224f3af84eef90c4d0122d76
Muhammad Vidi <vidim@impstudio.co.id>
2025-05-25 19:56:30 +0700

    create migrations for fixing tables

---
+++ migrations/20250525081947_alter_users_table_add_email_column.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  return await knex.schema.alterTable('users', table => {
+    table.string('email').notNullable()
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  return await knex.schema.alterTable('users', table => {
+    table.dropColumn('email')
+  })
+};
---
+++ migrations/20250525124448_alter_movie_table_add_image_column.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  return await knex.schema.alterTable('movies', table => {
+    table.string('image_path')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  return await knex.schema.alterTable('movies', table => {
+    table.dropColumn('image_path')
+  })
+};
